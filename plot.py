ac9d28462847c7fb73a419fd9371355658371e4c
Daniil Iskhakov <shkiper326@outlook.com>
2025-12-29 21:13:18 +0300

    Disable PNG plotting in favor of TensorBoard visualization

--- plot.py
+++ plot.py
@@ -4,17 +4,27 @@ import os
 import matplotlib.pyplot as plt
 import matplotlib
 
+# PNG plotting disabled - use TensorBoard instead
+# Run `./start_tensorboard.sh` to visualize all metrics
+
 if __name__ == '__main__':
-    fd = open(os.path.join('result.json'), 'r')
-    json_content = json.loads(fd.read())
-    fd.close()
+    print("PNG plotting is disabled.")
+    print("Use TensorBoard for visualization:")
+    print("  1. Run: ./start_tensorboard.sh")
+    print("  2. Open: http://localhost:6006")
+    print("")
+    print("All metrics are available in TensorBoard after running train.py or stats.py")
 
-    foods = json_content['mean_food']
-    tsses = json_content['states']
+    # fd = open(os.path.join('result.json'), 'r')
+    # json_content = json.loads(fd.read())
+    # fd.close()
 
-    plt.clf()
-    matplotlib.rcParams.update({'font.size': 22})
-    plt.figure(figsize=(25, 20), dpi=80)
-    plt.plot(tsses, foods)
-    plt.savefig('scores.png')
+    # foods = json_content['mean_food']
+    # tsses = json_content['states']
+
+    # plt.clf()
+    # matplotlib.rcParams.update({'font.size': 22})
+    # plt.figure(figsize=(25, 20), dpi=80)
+    # plt.plot(tsses, foods)
+    # plt.savefig('scores.png')
     
